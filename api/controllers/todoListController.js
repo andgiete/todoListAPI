858d9616409aa28fd7852d452730d3c2de164b7d
andgiete <simonsaliba96@gmail.com>
2018-08-22 09:38:55 +0300

    Added Authentication for api with only username:simon and password: saliba

--- api/controllers/todoListController.js
+++ api/controllers/todoListController.js
@@ -2,8 +2,22 @@
 
 var mongoose = require('mongoose');
 var Task = mongoose.model('Tasks');
+var jwt = require('jsonwebtoken');
+
+function verify(token,secret){
+  try{
+    var decoded = jwt.verify(token,secret);
+  }catch(err){
+    return false;
+  }
+  return decoded;
+};
 
 exports.list_all_tasks = function(req, res){
+  if(!req.headers.authorization || !verify(req.headers.authorization,'secret')){
+    res.json({message:'Not Authenticated'});
+    return;
+  }
   Task.find({},function(err,task){
     if(err)
       res.send(err);
@@ -12,6 +26,10 @@ exports.list_all_tasks = function(req, res){
 };
 
 exports.create_a_task = function(req, res){
+  if(!req.headers.authorization || !verify(req.headers.authorization,'secret')){
+    res.json({message:'Not Authenticated'});
+    return;
+  }
   var new_task = new Task(req.body);
   new_task.save(function(err,task){
     if(err)
@@ -21,6 +39,11 @@ exports.create_a_task = function(req, res){
 };
 
 exports.read_a_task = function(req, res){
+  console.log(req);
+  if(!req.headers.authorization || !verify(req.headers.authorization,'secret')){
+    res.json({message:'Not Authenticated'});
+    return;
+  }
   Task.findById(req.params.taskId, function(err, task){
     if(err)
       res.send(err);
@@ -29,6 +52,10 @@ exports.read_a_task = function(req, res){
 };
 
 exports.update_a_task = function(req, res){
+  if(!req.headers.authorization || !verify(req.headers.authorization,'secret')){
+    res.json({message:'Not Authenticated'});
+    return;
+  }
   Task.findOneAndUpdate({_id: req.params.taskId}, req.body, {new: true}, function(err, task){
     if(err)
       res.send(err);
@@ -37,6 +64,10 @@ exports.update_a_task = function(req, res){
 };
 
 exports.delete_a_task = function(req, res){
+  if(!req.headers.authorization || !verify(req.headers.authorization,'secret')){
+    res.json({message:'Not Authenticated'});
+    return;
+  }
   Task.remove({
     _id: req.params.taskId
   },function(err,task){
@@ -45,3 +76,16 @@ exports.delete_a_task = function(req, res){
     res.json({message:'Task successfully deleted.'});
   });
 };
+
+exports.authenticate = function(req,res){
+  if(req.body.username == 'simon' && req.body.password == 'saliba'){
+    var token = jwt.sign({ name: 'Simon Saliba' , iat: Date.now()}, 'secret',function(err,mytoken){
+      if(err)
+        res.send(err);
+      console.log(mytoken);
+      res.json({token: mytoken});
+    });
+  }else{
+    res.json({message:'Invalid username or password'});
+  }
+}
